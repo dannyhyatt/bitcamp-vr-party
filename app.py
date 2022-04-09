0ec6ead4444c3c36ba6675a7c11241c2d1b303a8
Daniel Hyatt <danny@dannysmacbookair.local>
2022-04-09 06:50:02 -0400

    finished the server?

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from email.policy import default
-from flask import Flask
+from flask import Flask, Blueprint
+from flask_sockets import Sockets
 from flask import request
 import psycopg2
+import random, string
 
 print(1)
 conn = psycopg2.connect(
@@ -18,7 +19,41 @@ cur.execute('SELECT version()')
 db_version = cur.fetchone()
 print(db_version)
 
+html = Blueprint(r'html', __name__)
+ws = Blueprint(r'ws', __name__)
+
+rooms = {}
+
+@html.route('/')
+def hello():
+    return 'Hello World!'
+
+@ws.route('/echo')
+def echo_socket(socket):
+    while not socket.closed:
+        message = str(socket.receive())
+        if message.startswith('create_room:'):
+            # create_room:<username>
+            code = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(6))
+            rooms[code] = [{'username' : message.replace('create_room:', ''), 'socket' : socket}]
+            socket.send(code)
+        elif message.startswith('join_room:'):
+            # join_room:<username>:<code>
+            rooms[message.split(':')[2]].append({'username' : message.split(':')[1], 'socket' : socket})
+            
+            socket.send('{}'.format(list(map(lambda x: x['username'], rooms[message.split(':')[2]]))))
+        else:
+            # <code>:<username>:<message>
+            code = message.split(':')[0]
+            username = message.split(':')[1]
+            message = message.split(':')[2]
+            for sock in rooms[code]:
+                sock['socket'].send(message)
+        print(rooms)
+
+
 app = Flask(__name__)
+sockets = Sockets(app)
 
 @app.route("/")
 def hello_world():
@@ -42,4 +77,13 @@ def signup():
     success = cur.execute(query)
     return 'success: {}'.format(success)
 
-app.run(debug=True)
+app.register_blueprint(html, url_prefix=r'/')
+
+sockets.register_blueprint(ws, url_prefix=r'/')
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('127.0.0.1', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
--- testserver.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-
-app = Flask(__name__,
-            static_url_path='', 
-            static_folder='')
-
-app.run(ssl_context=('cert.pem', 'key.pem'))
